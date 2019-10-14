0936c98998d9142aba2348e0402384ec55c465b8
marioalbornoz <mario.albornozc@utem.cl>
2019-10-14 17:53:37 -0300

    primer sitio web con node

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// setting
+app.set('port', 3000);
+
+// middlewares
+
+// routes
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+// static files
+
+// listening the server
+app.listen(app.get('port'), () =>{
+    console.log('Server on port', app.get('port'));     
+});
