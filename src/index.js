ceed7c5ac8052b56d80c5ee1c2198eb27f89d323
marioalbornoz <mario.albornozc@utem.cl>
2019-10-15 23:31:10 -0300

    cambios menores

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+
 // setting
 app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // middlewares
 
 // routes
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
-    
+    //res.sendFile(path.join(__dirname, 'views/index.html'));
+    res.render('index', { title: 'First Web node' });
 });
 
 // static files
